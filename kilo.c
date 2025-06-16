86965d9a6d55aea8188fee1d9cb52105102250ef
ola-iya <bancolejeandedieu@gmail.com>
2025-06-16 12:35:28 +0100

    feat: arrow things

--- kilo.c
+++ kilo.c
@@ -14,6 +14,12 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define KILO_VERSION "0.0.1 by Ola-Iya"
 
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN
+};
 
 
 
@@ -123,7 +129,7 @@ void abFree(struct abuf *ab)
 }
 
 /* terminal */
-char editorReadKey()
+int  editorReadKey()
 {
 	int nread;
 	char c;
@@ -131,27 +137,57 @@ char editorReadKey()
 	{
 		if (nread == 1 && errno != EAGAIN)
 			die("read");
-
 	}
-	return c;
+
+	if (c == '\x1b')
+	{
+		char seq[3];
+		
+		if(read(STDIN_FILENO, &seq[0], 1) != 1)
+			return '\x1b';
+		if(read(STDIN_FILENO, &seq[1], 1) != 1)
+			return '\x1b';
+
+		if(seq[0] == '[')
+		{
+			switch (seq[1])
+			{
+				case 'A':
+					return ARROW_UP;
+				case 'B':
+					return ARROW_DOWN;
+				case 'C':
+					return ARROW_RIGHT;
+				case 'D':
+					return ARROW_LEFT;
+			}
+
+		}
+		return '\x1b';
+	}
+	else
+	{
+		return c;
+	}
+
 }
 
 /* input */
 
-void editorMoveCursor(char key)
+void editorMoveCursor(int key)
 {
 	switch (key)
 	{
-		case 'a':
+		case ARROW_LEFT:
 			E.cx--;
 			break;
-		case 'd':
+		case ARROW_RIGHT:
 			E.cx++;
 			break;
-		case 'w':
+		case ARROW_UP:
 			E.cy--;
 			break;
-		case 's':
+		case ARROW_DOWN:
 			E.cy++;
 			break;
 	}
@@ -160,7 +196,7 @@ void editorMoveCursor(char key)
 
 void editorProcessKeypress()
 {
-	char c = editorReadKey();
+	int c = editorReadKey();
 
 	switch (c) {
 		case CTRL_KEY('q'):
@@ -168,10 +204,10 @@ void editorProcessKeypress()
 			write(STDOUT_FILENO, "\x1b[H", 3); //
 			exit(0);
 			break;
-		case 'w':
-		case 's':
-		case 'a':
-		case 'd':
+		case ARROW_UP:
+		case ARROW_DOWN:
+		case ARROW_LEFT:
+		case ARROW_RIGHT:
 			editorMoveCursor(c);
 			break;
 	}
